f7d650a57915fadf3d51a959bb99a286d5dd3b8d
Maxlego08 <maxlego08@sfr.fr>
2021-01-08 12:24:30 +0100

    Update EntityDeathListener and EconomyHook

--- src/main/java/net/mackenziemolloy/MobCoins/hooks/EconomyHook.java
+++ src/main/java/net/mackenziemolloy/MobCoins/hooks/EconomyHook.java
@@ -8,61 +8,61 @@ import org.bukkit.entity.Player;
 
 import net.brcdev.shopgui.provider.economy.EconomyProvider;
 import net.mackenziemolloy.mobcoins.MobCoins;
+import net.mackenziemolloy.mobcoins.api.MobCoin;
 import net.mackenziemolloy.mobcoins.utils.CoinManager;
 
 public class EconomyHook extends EconomyProvider {
 
-    MobCoins mobCoins = MobCoins.getInstance();
+	private final MobCoins mobCoins = MobCoins.getInstance();
+	private final MobCoin mobCoin;
 
-    private Map<UUID, Double> playerBalances;
+	private Map<UUID, Double> playerBalances;
 
-    public EconomyHook() {
+	public EconomyHook() {
 
-        // (Optional, default ones are empty strings)
-        // Override fields from superclass to set currency prefix & suffix
-        this.currencyPrefix = "";
-        this.currencySuffix = " MobCoins";
+		// (Optional, default ones are empty strings)
+		// Override fields from superclass to set currency prefix & suffix
+		this.currencyPrefix = "";
+		this.currencySuffix = " MobCoins";
 
-        this.playerBalances = new HashMap<>();
-    }
+		this.playerBalances = new HashMap<>();
+		this.mobCoin = this.mobCoins.getMobCoin();
+	}
 
-    @Override
-    public String getName() {
-        return "MobCoins";
-    }
+	@Override
+	public String getName() {
+		return "MobCoins";
+	}
 
+	@Override
+	public double getBalance(Player player) {
 
-    @Override
-    public double getBalance(Player player) {
+		if (mobCoins.dataFile.get(player.getUniqueId().toString()) != null) {
 
+			return mobCoins.dataFile.getDouble(player.getUniqueId().toString());
 
-        if(mobCoins.dataFile.get(player.getUniqueId().toString()) != null) {
+		}
 
-            return mobCoins.dataFile.getDouble(player.getUniqueId().toString());
+		else {
 
-        }
+			return 0.0;
 
-        else {
+		}
 
-            return 0.0;
+	}
 
-        }
+	@Override
+	public void deposit(Player player, double amount) {
 
-    }
+		this.mobCoin.addMobCoins(player, amount);
 
-    @Override
-    public void deposit(Player player, double amount) {
+	}
 
-        CoinManager.addMobCoins(player, amount);
+	@Override
+	public void withdraw(Player player, double amount) {
 
+		this.mobCoin.removeMobCoins(player, amount);
 
-    }
-
-    @Override
-    public void withdraw(Player player, double amount) {
-
-        CoinManager.removeMobCoins(player, amount);
-
-    }
+	}
 
 }
